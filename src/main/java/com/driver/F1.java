796738334fee1fa8f6db0a7d19007fddb831b317
DELL <ankitkumaar1302@gmail.com>
2023-08-27 02:16:50 +0530

    Vehicles_Solution

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,6 +3,8 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
+        super(name,4,4,3,isManual,"BMW",2);
+
         //Use arbitrary values for parameters which are not mentioned
     }
 
@@ -20,8 +22,28 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            changeGear(1);
         }
         //for all other cases, change the gear accordingly
+        else if(newSpeed <= 50){
+            changeGear(1);
+        }
+        else if(newSpeed <= 100){
+            changeGear(2);
+        }
+        else if (newSpeed <= 150) {
+            changeGear(3);
+        }
+        else if(newSpeed <= 200){
+            changeGear(4);
+        }
+        else if (newSpeed <= 250) {
+            changeGear(5);
+        }
+        else {
+            changeGear(6);
+        }
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
